0f216c5fac1c6181f27dd1c4c035c20f777791fa
ziyi2112 <ziyi2112@gmail.com>
2018-11-20 17:30:43 +0900

    Remove fazzy code

--- src/app/ex-grid/ex-grid.component.ts
+++ src/app/ex-grid/ex-grid.component.ts
@@ -1,8 +1,6 @@
 import {
   Component,
-  HostListener,
   ViewChild,
-  AfterViewInit
 } from '@angular/core';
 import { IgxGridComponent } from 'igniteui-angular';
 import { athletesData } from './data';
@@ -12,24 +10,9 @@ import { athletesData } from './data';
   templateUrl: './ex-grid.component.html',
   styleUrls: ['./ex-grid.component.scss']
 })
-export class ExGridComponent implements AfterViewInit {
+export class ExGridComponent {
   data = athletesData;
 
   @ViewChild('mygrid')
   mygrid: IgxGridComponent;
-
-  @HostListener('window:resize', ['$event'])
-  public onResize(event) {
-    console.log(event);
-  }
-
-  @HostListener('window:load', ['$event'])
-  public onLoad(event) {
-    console.log(event);
-  }
-
-  public ngAfterViewInit() {
-    console.log('AfterViewInit');
-    console.log(this.mygrid.height);
-  }
 }
